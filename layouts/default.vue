e6541826939a17e4a0feb05093d8f9f083331f89
natchi22 <nat.51124@gmail.com>
2020-09-30 18:02:42 +0700

    update line liff

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,41 @@
 <template>
   <div class="layout-margin">
     <Nuxt />
+    <img :src="image" alt="">
   </div>
 </template>
 <style>
     @import '../style/style.css';
 </style>
+<script>
+import liff from '@line/liff'
+
+export default {
+  data () {
+    return {
+      image: '',
+      name:'',
+      userId:''        
+    }
+  },
+  mounted () {
+    liff.init({ liffId: '1654989800-V2XxzW4z' })
+      .then(() => {
+        if (liff.isLoggedIn()) {
+          liff.getProfile().then((profile) => {
+            const userId = profile.userId
+            const pictureUrl = profile.pictureUrl
+            const name = profile.displayName
+            this.userId = userId
+            this.name = name
+            this.image = image
+            alert(userId+"----"+name+"----"+img)
+          })
+        }
+        else{
+          liff.login()
+        }
+      })
+  }
+}
+</script>
