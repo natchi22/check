002a09ccb38e1442ac76a1f55f992339fa1f92e1
natchi22 <nat.51124@gmail.com>
2020-10-02 14:08:52 +0700

    test

--- pages/index.vue
+++ pages/index.vue
@@ -11,52 +11,52 @@
 </template>
 
 
-<script>
-import liff from '@line/liff'
-import { mapMutations } from 'vuex'
-export default {
-  data () {
-    return {
-      image: '',
-      name:'',
-      userId:''        
-    }
-  },
-  methods:{
-    ...mapMutations({
-        saveProfile: 'profile/saveProfile'
-      }),
-  },
-  mounted () {
-    const data = {
-      img : this.image,
-      id : this.userId,
-      name : this.name
-    }
-    liff.init({ liffId: '1654989800-V2XxzW4z' })
-      .then(() => {
-        if (liff.isLoggedIn()) {
-          liff.getProfile().then((profile) => {
-            const userId = profile.userId
-            const image = profile.pictureUrl
-            const name = profile.displayName
-            this.userId = userId
-            this.name = name
-            this.image = image
-            this.saveProfile(profile)
-          })
-        }
-        else{
-          liff.login()
-        }
-      })
-      .catch((err) => {
-            alert('Connect failed, please try again.')
-            liff.closeWindow()
-        })
-  }
-}
-</script>
+// <script>
+// import liff from '@line/liff'
+// import { mapMutations } from 'vuex'
+// export default {
+//   data () {
+//     return {
+//       image: '',
+//       name:'',
+//       userId:''        
+//     }
+//   },
+//   methods:{
+//     ...mapMutations({
+//         saveProfile: 'profile/saveProfile'
+//       }),
+//   },
+//   mounted () {
+//     const data = {
+//       img : this.image,
+//       id : this.userId,
+//       name : this.name
+//     }
+//     liff.init({ liffId: '1654989800-V2XxzW4z' })
+//       .then(() => {
+//         if (liff.isLoggedIn()) {
+//           liff.getProfile().then((profile) => {
+//             const userId = profile.userId
+//             const image = profile.pictureUrl
+//             const name = profile.displayName
+//             this.userId = userId
+//             this.name = name
+//             this.image = image
+//             this.saveProfile(profile)
+//           })
+//         }
+//         else{
+//           liff.login()
+//         }
+//       })
+//       .catch((err) => {
+//             alert('Connect failed, please try again.')
+//             liff.closeWindow()
+//         })
+//   }
+// }
+// </script>
 <style>
 
 </style>
