ed7fb5c3cd3167f60d72cb4fd82e3de55ef4271b
natchi22 <nat.51124@gmail.com>
2020-10-12 16:22:46 +0700

    date

--- pages/freelance/history/index.vue
+++ pages/freelance/history/index.vue
@@ -36,10 +36,11 @@ export default {
 
 		}) //เรียกมาโชว์ doc=กลุ่มdataหน้าinput
 		
-		const dateTime = await this.$fireStore.collection("Task").where("freelanceId",'==',  this.freelanceData.freelanceId).get()
+		const dateTime = await this.$fireStore.collection("Task").orderByChild('dateIn').where("freelanceId",'==',  this.freelanceData.freelanceId)
+		.get()
 		dateTime.forEach((doc)=>{
 			this.tasks.push(doc.data())
-		}) //เรียกมาโชว์ doc=กลุ่มdataหน้าinput
+		})
 		console.log(this.showDateTime)
   	}
 }
