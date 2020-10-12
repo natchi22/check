76ec941563f73628310a9800be60f4a7700eab14
natchi22 <nat.51124@gmail.com>
2020-10-12 16:26:17 +0700

    ds

--- pages/freelance/history/index.vue
+++ pages/freelance/history/index.vue
@@ -36,8 +36,8 @@ export default {
 
 		}) //เรียกมาโชว์ doc=กลุ่มdataหน้าinput
 		
-		const dateTime = await this.$fireStore.collection("Task").orderByChild('dateIn').where("freelanceId",'==',  this.freelanceData.freelanceId)
-		.get()
+		const dateTime = await this.$fireStore.collection("Task").where("freelanceId",'==',  this.freelanceData.freelanceId)
+		.get().orderByChild('dateIn')
 		dateTime.forEach((doc)=>{
 			this.tasks.push(doc.data())
 		})
