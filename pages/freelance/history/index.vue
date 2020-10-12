5ec8b4f2dd7dbaa46e4baa35e459b8b004fb7bad
natchi22 <nat.51124@gmail.com>
2020-10-12 15:08:25 +0700

    j

--- pages/freelance/history/index.vue
+++ pages/freelance/history/index.vue
@@ -28,15 +28,15 @@ export default {
 	async mounted(){
 		// .where freelanceId=ตัวที่อ่านค่า หัวข้อมูลกลุ่มนั้น อยู่หน้าที่inputมา,== ไอดีไหน,ไอดีที่จะเอามา อันนี้ระบุเป็นตัวแต่เดี๋ยวต้องระบุobject id
 		const freelance = await this.$fireStore.collection("Freelance").where("lineId",'==', this.profile.userId ).get()
-		freelance.map((doc)=>{
+		freelance.forEach((doc)=>{
 			this.freelanceData = doc.data()
+			
 		}) //เรียกมาโชว์ doc=กลุ่มdataหน้าinput
 		
 		const dateTime = await this.$fireStore.collection("Task").where("freelanceId",'==',  this.freelanceData.freelanceId).get()
-		this.showDateTime = dateTime
-		// dateTime.forEach((doc)=>{
-		// 	this.showDateTime = doc.data()
-		// }) //เรียกมาโชว์ doc=กลุ่มdataหน้าinput
+		dateTime.forEach((doc)=>{
+			this.showDateTime = doc.data()
+		}) //เรียกมาโชว์ doc=กลุ่มdataหน้าinput
 		console.log(this.showDateTime)
   	}
 }
