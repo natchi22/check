8d5460d935ca04fc71d94f3b1eab47dab098aa4f
natchi22 <nat.51124@gmail.com>
2021-03-03 23:25:37 +0700

    ลอง หน้าเข้าระบเออเร่อ

---
+++ pages/manager/index.vue
@@ -0,0 +1,72 @@
+<template>
+	<div class="login">
+		<h1>เข้าสู่ระบบ</h1>
+        <a-input 
+            class="input"
+            placeholder="ชื่อผู้ใช้" 
+            v-model="user" 
+        />
+        <a-input-password 
+            class="input"
+            placeholder="รหัสผ่าน" 
+            v-model="password" 
+        />
+		
+		<button class="btn btn-green" @click="login" >
+			เข้าสู่ระบบ
+		</button>
+        <nuxt-link to="/manager/register">
+            <button class="btn btn-green" @click="register" >
+                สมัครสมาชิก
+            </button>
+        </nuxt-link>
+	</div>
+</template>
+<script>
+// import { mapState,mapMutations } from 'vuex'
+export default {
+    data(){
+        return{
+            user: '',
+            password: ''
+        }
+    },
+    // methods:{
+    //     async login(e) {
+    //         e.preventDefault();
+    //         if (this.user && this.password) {
+    //             try {
+    //                 await this.$fireAuth.signInWithEmailAndPassword(this.user,  this.password);
+    //                 this.$router.push('/admin/history');
+    //             } catch (e) {
+    //                 this.$router.push('/admin');
+    //             }
+    //         }
+
+    //     }
+        
+    // }
+}
+</script>
+<style scoped>
+.login{
+    display: flex;
+    flex-direction: column;
+    width: 245px;
+    margin: 0 auto;
+    color: #000000;
+}
+.login h1{
+    font-size: 36px;
+    font-weight: bold;
+    color: #3ABCA7;
+    margin: 100px 0 26px 0;
+    text-align: center;
+}
+.input{
+    margin: 0 0 16px 0;
+}
+.btn-green{
+    margin: 0 0 16px 0;
+}
+</style>
---
+++ pages/manager/register/index.vue
@@ -0,0 +1,121 @@
+<template>
+	<div class="login">
+		<h1>สมัครสมาชิก</h1>
+        <div class="div-pic">
+            <img 
+                class="pic size-pic" 
+                :src="profile.pictureUrl" 
+                alt="รูปโปรไฟล์"
+            >
+        </div>
+        <h2>Username</h2>
+		<input 
+            type="text" 
+            placeholder="ชื่อผู้ใช้" 
+            v-model="user">
+        <h2>Password</h2>
+		<input 
+            type="text" 
+            placeholder="รหัสผ่าน" 
+            v-model="password"
+        >
+        <h2>ชื่อ</h2>
+        <input 
+            type="text" 
+            placeholder="ชื่อ" 
+            v-model="fName"
+        >
+        <h2>นามสกุล</h2>
+        <input 
+            type="text" 
+            placeholder="นามสกุล" 
+            v-model="lName"
+        >
+        <h2>เบอร์โทรศัพท์</h2>
+        <input 
+            type="text" 
+            placeholder="เบอร์โทรศัพท์" 
+            v-model="telNumber"
+        >
+        <h2>E-mail</h2>
+        <input
+            type="text"
+            placeholder="E-mail*"
+            v-model="email"
+        >
+        <button 
+            class="btn btn-green" 
+            @click="register" 
+        >
+            สมัครสมาชิก
+        </button>
+	</div>
+</template>
+<script>
+import { mapState, mapMutations } from 'vuex'
+export default {
+    data(){
+        return{
+            user:'',
+            password:'',
+            fName: '',
+            lName: '',
+            email: '',
+            lineId: '',
+			telNumber: '',
+        }
+    },
+    computed: { //นำstoreไปใช้ วางไว้หน้าที่จะใช้ และเรียกใช้บนโค้ด **import mapState ด้วย
+        ...mapState({
+            profile: state => state.profile.profileData
+        })
+    },
+    async mounted() {
+
+    },
+    methods:{
+        async register() {
+            const user = this.$fireStore.collection("Manager").doc()
+            await user.set({
+                freelanceId: user.id,
+                firstName: this.fName,
+                lastName: this.lName,
+                phone: this.telNumber,
+                email: this.email,
+                lineId: this.profile.userId,
+                pictureUrl: this.profile.pictureUrl
+            })
+            console.log(user)
+        },
+        
+    }
+}
+</script>
+<style scoped>
+.login{
+    display: flex;
+    flex-direction: column;
+    width: 245px;
+    margin: 0 auto;
+    text-align: center;
+    color: #000000;
+}
+.login h1{
+    font-size: 36px;
+    font-weight: bold;
+    color: #3ABCA7;
+    margin: 30px 0 16px 0;
+}
+h2{
+    text-align: left;
+}
+.div-pic{
+    display: flex;
+    justify-content: center;
+    margin: 0 0 16px 0;
+}
+.size-pic{
+    width: 80px;
+    height: 80px;
+}
+</style>
