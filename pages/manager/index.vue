7497577b7580a2141681019ee0f8d55f1191283d
natchi22 <nat.51124@gmail.com>
2021-03-04 22:23:10 +0700

    แก้ manager เพิ่มliff

--- pages/manager/index.vue
+++ pages/manager/index.vue
@@ -58,6 +58,36 @@ export default {
             
         }
     },
+    async mounted () {
+        // const data = {
+        //     img: this.image,
+        //     id: this.userId,
+        //     name: this.name
+        // }
+        liff.init({ liffId: '1655688087-m6GLXp2q' })
+            .then(() => {
+                if (liff.isLoggedIn()) {
+                    // const queryString = decodeURIComponent(window.location.search).replace('?liff.state=', '')
+                    // const params = new URLSearchParams(queryString)
+                    liff.getProfile().then(async (profile) => {
+                        const userId = profile.userId /// เอาuser id line จากที่ได้ตรงนี้ไปเก็บใน firestore get เข้าไปพร้อมกับหน้าfreelance index
+                        const image = profile.pictureUrl
+                        const name = profile.displayName
+                        this.userId = userId
+                        this.name = name
+                        this.image = image
+                        this.saveProfile(profile)
+                    })
+                }
+                else {
+                    liff.login()
+                }
+            })
+            .catch(() => {
+                alert('Connect failed, please try again.')
+                liff.closeWindow()
+            })
+    }
     // methods:{
     //     async login(e) {
     //         e.preventDefault();
