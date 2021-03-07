f2e1f8551308fee2f4e589407b212ded8df41edf
natchi22 <nat.51124@gmail.com>
2021-03-07 15:11:36 +0700

    ปุ่มloginไม่ลิ้ง

--- pages/manager/login/index.vue
+++ pages/manager/login/index.vue
@@ -48,19 +48,19 @@ export default {
         //     saveProfile: 'profile/saveProfile'
         // }),
 
-        async login(e) {
-            e.preventDefault()
-            if (this.email && this.password) {
-                try {
-                    await this.$fireAuth.signInWithEmailAndPassword(this.user, this.password)
-                    this.$router.push(`/manager/${this.profile.userId}`)
-                }
-                catch (e) {
-                    this.$router.push('/manager')
-                }
-            }
+        // async login(e) {
+        //     e.preventDefault()
+        //     if (this.email && this.password) {
+        //         try {
+        //             await this.$fireAuth.signInWithEmailAndPassword(this.user, this.password)
+        //             this.$router.push(`/manager/${this.profile.userId}`)
+        //         }
+        //         catch (e) {
+        //             this.$router.push('/manager')
+        //         }
+        //     }
 
-        }
+        // }
     },
     watch: { //เซฟไลน์เข้า DB
         async profile() { //ต่อไฟเบสเข้า คอลเลคชัน freelance ถ้าเจอ lineId = profile.userId(?) เหมือนกัน get ข้อมูลออกมา
