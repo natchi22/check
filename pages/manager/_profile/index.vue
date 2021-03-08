10180975ab16f4b34dfebd760c9e1264593ee27b
natchi22 <nat.51124@gmail.com>
2021-03-08 23:28:36 +0700

    link ไปหน้างานแต่ละคน

--- pages/manager/_profile/index.vue
+++ pages/manager/_profile/index.vue
@@ -44,7 +44,7 @@
                     key="2"
                     tab="รายชื่อ"
                 >
-                    <nuxt-link to="/head/foremen/checkEmploy">
+                    <nuxt-link :to="`/freelance/task/${task.taskId}`">
                         <div
                             class="box"
                             v-for="item in inforFreelance"
