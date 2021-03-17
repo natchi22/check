6e5d2362999c1695c1b6a8dcf4161c80a445e658
Kunjanaphorn Boonmak <kunjanaphorn.b@gmail.com>
2021-03-17 22:15:50 +0700

    log

--- pages/manager/_profile/index.vue
+++ pages/manager/_profile/index.vue
@@ -2,7 +2,7 @@
     <div class="body">
         <div class="profile-head">
             <!-- กดรูปดูโปรไฟล์ -->
-            <h2>{{ inforManager.firstName }}  {{ inforManager.lastName }}</h2>
+            <h2>{{ inforManager }}  {{ inforManager }}</h2>
             <nuxt-link :to="`/manager/${profile.userId}/info`">
                 <img
                     class="pic size-pic"
