4d47d0a881facdf0144f2ff6e86a7ce485069449
Kunjanaphorn Boonmak <kunjanaphorn.b@gmail.com>
2021-03-20 11:22:38 +0700

    fix

--- pages/freelance/_profile/info.vue
+++ pages/freelance/_profile/info.vue
@@ -3,7 +3,7 @@
         <div class="profile-img">
             <img
                 class="pic size-pic"
-                :src="profile.pictureUrl"
+                :src="inforFrelance.pictureUrl"
                 alt="รูปโปรไฟล์"
             >
         </div>
